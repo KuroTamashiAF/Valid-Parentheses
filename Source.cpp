17619ac00a3c5a3e16ee186474e72f1bc5c766ee
Fedorov <chugun777akk@mail.ru>
2023-03-19 18:08:00 +0700

    create source

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 class Solution {
 public:
     static bool isValid(string s) {
 
+        if (s[0] == ']' || s[0] == '}' || s[0] == ')')
+            return false;
+        int result = 0;
+        map<char, int> vocab;
+        vocab['['] = 3;
+        vocab['('] = 2;
+        vocab['{'] = 1;
+        vocab[']'] = -3;
+        vocab[')'] = -2;
+        vocab['}'] = -1;
+        for (int i = 0; i < s.size(); i++)
+        {
+            result += vocab[s[i]];
+        }
+        if (result == 0)
+            return true;
+        else
+            return false;
 
 
     }
@@ -14,7 +33,8 @@ public:
 
 int main()
 {
-    string input = "()[]{}";
-    Solution::isValid(input);
+    string input = "(]";
+    cout << Solution::isValid(input) << endl;
+    return 0;
    
 }
